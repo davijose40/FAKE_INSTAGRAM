d9f2d330f3429d91a03212799d54a4a5cadc4358
davijose <davijose40@gmail.com>
2020-05-01 13:24:44 -0300

    find user on DB and create session, ready-to-go

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,42 @@ const config = require('../configs/database');
 const bcrypt = require('bcrypt');
 
 const authController = {
+  // view to get login info
   create: ( _req, res ) => {
     return res.render("auth/login");
   },
 
+  // business rules to login
   store: async (req, res) => {
+    // get params
     const { email, password } = req.body;
+    // connect DB
     const con = new Sequelize(config);
-  },
+    // find user
+    const [user] = await con.query(
+      "SELECT * FROM users WHERE email= :email", {
+      replacements: {
+        email,
+      },
+      type: Sequelize.QueryTypes.SELECT,
+    });
+
+    if(!user || !bcrypt.compareSync(password, user.password)) {
+      return res.render("auth/login", {
+        msg: "Email ou Senha invalidos!",
+      });
+    }
+
+    req.session.user = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    return res.redirect("/home");
+
+  }, // end store
+  
 };
 
 module.exports = authController;
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,19 +4,19 @@ const bcrypt = require('bcrypt');
 
 const userController = {
 
-  /* call auth register to add users information */
+  /* entry point and register view to add users information */
   create: (req, res) => res.render("auth/register"),
-
+  
   /* store user signin */
   store: async (req, res) => {
     const {name, email, username, password} = req.body;
     const con = new Sequelize(config);
     const hashPassword = bcrypt.hashSync(password, 10);
-
+    
     const user = await con.query(
       "INSERT INTO users (name, username, email, password, create_at, update_at) VALUES(:name, :username, :email, :password, :create_at, :update_at)", 
-      {
-        replacements: {
+      { // object passed after query
+        replacements: { // replacement passed by req.body and date
           name,
           email,
           username,
@@ -26,17 +26,20 @@ const userController = {
         },
         type: Sequelize.QueryTypes.INSERT,
       }
-    );
-    if (!user) {
-      return res.render("auth/register", {
-        msg: "Erro ao cadastrar um usario",
-      });
-    }
-
-    return res.redirect("/home");
-
-  }, // end store
-
+      );
+      if (!user) {
+        return res.render("auth/register", {
+          msg: "Erro ao cadastrar um usario",
+        });
+      }
+      
+      return res.redirect("/home");
+      
+    }, // end store
+    
+    /* call home to registers users */
+    auth: (req, res) => res.render("/home", {user:req.session}),
+    
 }; // end userController
 
 module.exports = userController;
